881e5dfb7b6ca2b38d0bf9e8c13850ef4a005485
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-02-15 01:15:43 +0000

    modring: only parallelize event execution across different modules

--- pkg/modring/modring.go
+++ pkg/modring/modring.go
@@ -44,44 +44,101 @@ func New(ownID t.ModuleID, ringSize int, params ModuleParams, logger logging.Log
 
 func (m *Module) ImplementsModule() {}
 
-func (m *Module) ApplyEvents(evts *events.EventList) (*events.EventList, error) {
-	return modules.ApplyEventsConcurrently(evts, m.applyEvent)
+func (m *Module) ApplyEvents(eventsIn *events.EventList) (*events.EventList, error) {
+	// Note: this method is similar to ApplyEvents, but it only parallelizes execution across diferent
+	// modules, not all events.
+	ownEventsIn, subEventsIn := m.splitEventsByDest(eventsIn)
+
+	results := make([]chan *events.EventList, len(subEventsIn))
+	errors := make([]chan error, len(subEventsIn))
+	for i := 0; i < eventsIn.Len(); i++ {
+		results[i] = make(chan *events.EventList)
+		errors[i] = make(chan error)
+	}
+
+	// Start one concurrent worker for each submodule
+	// Note that the processing starts concurrently for all eventlists, and only the writing of the results is synchronized.
+	for i := 0; i < len(subEventsIn); i++ {
+		go func(mod modules.PassiveModule, evs *events.EventList, j int) {
+
+			// Apply the input event, catching potential panics.
+			res, err := mod.ApplyEvents(evs)
+
+			// Write processing results to the output channels.
+			// Attention: Those (unbuffered) channels must be read by the aggregator in the same order
+			//            as they are being written here, otherwise the system gets stuck.
+			results[j] <- res
+			errors[j] <- err
+
+		}(m.ring[i], &subEventsIn[i], i)
+		i++
+	}
+
+	// The event processing results will be aggregated here.
+	var firstError error
+	var eventsOut *events.EventList
+
+	// the modring's events will be handled in the current go routine
+	eventsOut, firstError = modules.ApplyEventsSequentially(ownEventsIn, m.applyEvent)
+
+	// For each submodule, read the processing result from the common channels and aggregate it with the rest.
+	for i := 0; i < len(subEventsIn); i++ {
+
+		// Attention: The (unbuffered) errors and results channels must be read in the same order
+		//            as they are being written by the worker goroutines, otherwise the system gets stuck.
+
+		// Read results from common channel and add it to the accumulator.
+		if evList := <-results[i]; evList != nil {
+			eventsOut.PushBackList(evList)
+		}
+
+		// Read error from common channel.
+		// We only consider the first error, as ApplyEventsConcurrently only returns a single error.
+		// TODO: Explore possibilities of aggregating multiple errors in one.
+		if err := <-errors[i]; err != nil && firstError == nil {
+			firstError = err
+		}
+
+	}
+
+	// Return the resulting events or an error.
+	if firstError != nil {
+		return nil, firstError
+	}
+	return eventsOut, nil
+}
+
+func (m *Module) splitEventsByDest(eventsIn *events.EventList) (*events.EventList, []events.EventList) {
+	ownEventsIn := &events.EventList{}
+	subEventsIn := make([]events.EventList, len(m.ring))
+
+	eventsInIter := eventsIn.Iterator()
+	for event := eventsInIter.Next(); event != nil; event = eventsInIter.Next() {
+		if event.DestModule == m.ownID.Pb() {
+			ownEventsIn.PushBack(event)
+		} else {
+			idxStr := t.ModuleID(event.DestModule).StripParent(m.ownID).Top()
+			idx, err := strconv.ParseUint(string(idxStr), 10, 64)
+			if err != nil {
+				m.logger.Log(logging.LevelWarn, "event received for invalid submodule index", "submoduleIdx", idxStr)
+			}
+
+			subEventsIn[idx].PushBack(event)
+		}
+	}
+
+	return ownEventsIn, subEventsIn
 }
 
 func (m *Module) applyEvent(event *eventpb.Event) (*events.EventList, error) {
-	if t.ModuleID(event.DestModule) == m.ownID {
-		switch e := event.Type.(type) {
-		case *eventpb.Event_Init:
-			return events.EmptyList(), nil // Nothing to do at initialization.
-		case *eventpb.Event_Modring:
-			return m.applyModringEvent(e.Modring)
-		default:
-			return nil, fmt.Errorf("unsupported event type for modring module: %T", e)
-		}
+	switch e := event.Type.(type) {
+	case *eventpb.Event_Init:
+		return events.EmptyList(), nil // Nothing to do at initialization.
+	case *eventpb.Event_Modring:
+		return m.applyModringEvent(e.Modring)
+	default:
+		return nil, fmt.Errorf("unsupported event type for modring module: %T", e)
 	}
-	return m.forwardEvent(event)
-}
-
-func (m *Module) forwardEvent(event *eventpb.Event) (*events.EventList, error) {
-	subID := t.ModuleID(event.DestModule).StripParent(m.ownID).Top()
-
-	subIdx, err := strconv.ParseUint(string(subID), 10, 64)
-	if err != nil {
-		m.logger.Log(logging.LevelDebug, "message for bad submodule", "subID", subID)
-		return &events.EventList{}, nil
-	}
-
-	sub, evsOut, err := m.getSub(subIdx)
-	if err != nil {
-		return nil, err
-	}
-
-	moreEvsOut, err := sub.ApplyEvents(events.ListOf(event))
-	if err != nil {
-		return nil, err
-	}
-
-	return evsOut.PushBackList(moreEvsOut), nil
 }
 
 func (m *Module) getSub(subIdx uint64) (modules.PassiveModule, *events.EventList, error) {
