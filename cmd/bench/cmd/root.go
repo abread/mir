beb09ac3992a96962dc16a169570d7f5fa7da4f0
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-02-23 18:02:55 +0000

    cmd/bench: add profiling support

--- cmd/bench/cmd/root.go
+++ cmd/bench/cmd/root.go
@@ -5,7 +5,10 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/spf13/cobra"
 )
@@ -14,6 +17,8 @@ var (
 	id             string
 	membershipFile string
 	verbose        bool
+	cpuprofile     string
+	memprofile     string
 
 	rootCmd = &cobra.Command{
 		Use:   "bench",
@@ -25,9 +30,36 @@ var (
 )
 
 func Execute() {
+	if cpuprofile != "" {
+		f, err := os.Create(cpuprofile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not create CPU profile: %v", err)
+		}
+
+		defer f.Close()
+
+		if err := pprof.StartCPUProfile(f); err != nil {
+			fmt.Fprintf(os.Stderr, "could not start CPU profile: %v", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
+	}
+
+	if memprofile != "" {
+		f, err := os.Create(memprofile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not create memory profile: %v", err)
+		}
+
+		defer f.Close()
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			fmt.Fprintf(os.Stderr, "could not write memory profile: %v", err)
+		}
 	}
 }
 
@@ -37,4 +69,6 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&membershipFile, "membership", "m", "", "total number of nodes")
 	_ = rootCmd.MarkPersistentFlagRequired("membership")
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose mode")
+	rootCmd.PersistentFlags().StringVar(&cpuprofile, "cpuprofile", "", "write cpu profile to file")
+	rootCmd.PersistentFlags().StringVar(&memprofile, "memprofile", "", "write memory profile to file")
 }
