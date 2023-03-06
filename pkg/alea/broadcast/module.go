3b7c07bad6c8c993178f77de465ee4313190e37f
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-03-06 19:09:02 +0000

    alea/broadcast: reduce parallelism

--- pkg/alea/broadcast/module.go
+++ pkg/alea/broadcast/module.go
@@ -3,6 +3,7 @@ package broadcast
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"golang.org/x/exp/slices"
 
@@ -11,7 +12,6 @@ import (
 	"github.com/filecoin-project/mir/pkg/dsl"
 	"github.com/filecoin-project/mir/pkg/events"
 	"github.com/filecoin-project/mir/pkg/logging"
-	"github.com/filecoin-project/mir/pkg/modring"
 	"github.com/filecoin-project/mir/pkg/modules"
 	bcdsl "github.com/filecoin-project/mir/pkg/pb/aleapb/bcpb/dsl"
 	bcqueuedsl "github.com/filecoin-project/mir/pkg/pb/aleapb/bcqueuepb/dsl"
@@ -63,11 +63,74 @@ func NewModule(ctx context.Context, mc *ModuleConfig, params *ModuleParams, tuna
 		return nil, err
 	}
 
-	queues := modring.New(ctx, mc.Self, len(params.AllNodes), modring.ModuleParams{
-		Generator: newBcQueueGenerator(mc, params, tunables, nodeID, logger),
-	}, logging.Decorate(logger, "Modring controller: "))
+	queues, err := createQueues(ctx, mc, params, tunables, nodeID, logger)
+	if err != nil {
+		return nil, err
+	}
 
-	return modules.RoutedModule(mc.Self, controller, queues), nil
+	return &bcMod{
+		ownID:      mc.Self,
+		controller: controller,
+		queues:     queues,
+		logger:     logger,
+	}, nil
+}
+
+type bcMod struct {
+	ownID      t.ModuleID
+	controller modules.PassiveModule
+	queues     []modules.PassiveModule
+	logger     logging.Logger
+}
+
+func (m *bcMod) ImplementsModule() {}
+
+func (m *bcMod) ApplyEvents(evs *events.EventList) (*events.EventList, error) {
+	ownEvents, queueEvents := m.splitEvents(evs)
+
+	evsOut, err := m.controller.ApplyEvents(ownEvents)
+	if err != nil {
+		return nil, err
+	}
+
+	for qIdx, qEvs := range queueEvents {
+		if qEvs.Len() > 0 {
+			qEvsOut, err := m.queues[qIdx].ApplyEvents(qEvs)
+			if err != nil {
+				return nil, err
+			}
+
+			evsOut.PushBackList(qEvsOut)
+		}
+	}
+
+	return evsOut, nil
+}
+
+func (m *bcMod) splitEvents(evs *events.EventList) (*events.EventList, []*events.EventList) {
+	ownEvents := events.EmptyList()
+	queueEvents := make([]*events.EventList, len(m.queues))
+	for i := 0; i < len(queueEvents); i++ {
+		queueEvents[i] = events.EmptyList()
+	}
+
+	it := evs.Iterator()
+	for ev := it.Next(); ev != nil; ev = it.Next() {
+		if ev.DestModule == string(m.ownID) {
+			ownEvents.PushBack(ev)
+		} else {
+			dest := t.ModuleID(ev.DestModule).StripParent(m.ownID).Top()
+			idx, err := strconv.ParseInt(string(dest), 10, 64)
+			if err != nil || int(idx) >= len(queueEvents) || idx < 0 {
+				m.logger.Log(logging.LevelDebug, "event for invalid queue", "event", ev)
+				continue // invalid event
+			}
+
+			queueEvents[idx].PushBack(ev)
+		}
+	}
+
+	return ownEvents, queueEvents
 }
 
 func newQueueController(ctx context.Context, mc *ModuleConfig, params *ModuleParams, tunables *ModuleTunables, nodeID t.NodeID, logger logging.Logger) (modules.PassiveModule, error) {
@@ -79,6 +142,14 @@ func newQueueController(ctx context.Context, mc *ModuleConfig, params *ModulePar
 	}
 
 	ownQueueModID := mc.Self.Then(t.NewModuleIDFromInt(ownQueueIdx))
+
+	dsl.UponInit(m, func() error {
+		// init queues
+		for i := 0; i < len(params.AllNodes); i++ {
+			dsl.EmitEvent(m, events.Init(mc.Self.Then(t.NewModuleIDFromInt(i))))
+		}
+		return nil
+	})
 
 	bcdsl.UponStartBroadcast(m, func(queueSlot aleatypes.QueueSlot, txs []*requestpb.Request) error {
 		bcqueuedsl.InputValue(m, ownQueueModID, &commontypes.Slot{
@@ -101,14 +172,16 @@ func newQueueController(ctx context.Context, mc *ModuleConfig, params *ModulePar
 	return m, nil
 }
 
-func newBcQueueGenerator(queueMc *ModuleConfig, queueParams *ModuleParams, queueTunables *ModuleTunables, nodeID t.NodeID, logger logging.Logger) func(ctx context.Context, id t.ModuleID, idx uint64) (modules.PassiveModule, *events.EventList, error) {
+func createQueues(ctx context.Context, queueMc *ModuleConfig, queueParams *ModuleParams, queueTunables *ModuleTunables, nodeID t.NodeID, logger logging.Logger) ([]modules.PassiveModule, error) {
 	tunables := &bcqueue.ModuleTunables{
 		MaxConcurrentVcb: queueTunables.MaxConcurrentVcbPerQueue,
 	}
 
-	return func(ctx context.Context, id t.ModuleID, idx uint64) (modules.PassiveModule, *events.EventList, error) {
+	queues := make([]modules.PassiveModule, len(queueParams.AllNodes))
+
+	for idx := 0; idx < len(queueParams.AllNodes); idx++ {
 		mc := &bcqueue.ModuleConfig{
-			Self:         id,
+			Self:         queueMc.Self.Then(t.NewModuleIDFromInt(idx)),
 			Consumer:     queueMc.Self,
 			BatchDB:      queueMc.BatchDB,
 			Mempool:      queueMc.Mempool,
@@ -121,14 +194,16 @@ func newBcQueueGenerator(queueMc *ModuleConfig, queueParams *ModuleParams, queue
 			AllNodes:    queueParams.AllNodes,
 
 			QueueIdx:   aleatypes.QueueIdx(idx),
-			QueueOwner: queueParams.AllNodes[int(idx)],
+			QueueOwner: queueParams.AllNodes[idx],
 		}
 
 		mod, err := bcqueue.New(ctx, mc, params, tunables, nodeID, logging.Decorate(logger, "BcQueue: ", "queueIdx", idx))
 		if err != nil {
-			return nil, nil, err
+			return nil, err
 		}
 
-		return mod, events.EmptyList(), nil
+		queues[idx] = mod
 	}
+
+	return queues, nil
 }
