7066f31482bed1ea58e7b9355222bdac22a9cace
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-04-21 00:04:14 +0100

    Revert "events: switch from lists to slices to increase speed"
    This reverts commit 5861fcea904751e94b7e8c8c7dcf1d7763725998.

--- pkg/events/eventlist.go
+++ pkg/events/eventlist.go
@@ -7,14 +7,17 @@ SPDX-License-Identifier: Apache-2.0
 package events
 
 import (
-	"golang.org/x/exp/slices"
+	"container/list"
 
 	"github.com/filecoin-project/mir/pkg/pb/eventpb"
 )
 
 // EventList represents a list of Events, e.g. as produced by a module.
 type EventList struct {
-	evs []*eventpb.Event
+
+	// The internal list is intentionally left uninitialized until its actual use.
+	// This probably speeds up appending empty lists to other lists.
+	list *list.List
 }
 
 // EmptyList returns an empty EventList.
@@ -25,81 +28,95 @@ func EmptyList() *EventList {
 
 // ListOf returns EventList containing the given elements.
 func ListOf(events ...*eventpb.Event) *EventList {
-	return &EventList{
-		evs: events,
+	res := &EventList{}
+	for _, ev := range events {
+		res.PushBack(ev)
 	}
+	return res
 }
 
 // Len returns the number of events in the EventList.
 func (el *EventList) Len() int {
-	return len(el.evs)
+	if el.list == nil {
+		return 0
+	}
+	return el.list.Len()
 }
 
 // PushBack appends an event to the end of the list.
 // Returns the EventList itself, for the convenience of chaining multiple calls to PushBack.
-func (el *EventList) PushBack(event ...*eventpb.Event) *EventList {
-	if len(el.evs) == cap(el.evs) {
-		el.grow(len(event))
+func (el *EventList) PushBack(event *eventpb.Event) *EventList {
+	if el.list == nil {
+		el.list = list.New()
 	}
 
-	el.evs = append(el.evs, event...)
-
+	el.list.PushBack(event)
 	return el
 }
 
 // PushBackSlice appends all events in newEvents to the end of the current EventList.
 func (el *EventList) PushBackSlice(events []*eventpb.Event) *EventList {
-	return el.PushBack(events...)
+	if el.list == nil {
+		el.list = list.New()
+	}
+
+	for _, event := range events {
+		el.list.PushBack(event)
+	}
+
+	return el
 }
 
 // PushBackList appends all events in newEvents to the end of the current EventList.
 func (el *EventList) PushBackList(newEvents *EventList) *EventList {
-	return el.PushBack(newEvents.evs...)
-}
-
-const growCap int = 4096 - 32 // voices on the internet say allocation overhead hangs around 32B
-const growMin int = 32
-
-func (el *EventList) grow(szHint int) {
-	growSz := len(el.evs)
-	if growSz > growCap {
-		growSz = growCap
-	}
-	if growSz < szHint {
-		growSz = szHint
-	}
-	if growSz < growMin {
-		growSz = growMin
+	if newEvents.list != nil {
+		if el.list == nil {
+			el.list = list.New()
+		}
+		// TODO: Check out possible inefficiency. This implementation actually (shallowly) copies the elements from
+		//       newEvents.list to el.list. Most of the time it would be enough to simply wire together the two lists.
+		//       This would probably require a custom linked list implementation, since list.List does not seem to
+		//       support it.
+		el.list.PushBackList(newEvents.list)
 	}
 
-	el.evs = slices.Grow(el.evs, growSz)
+	return el
 }
 
 // Head returns the first up to n events in the list as a new list.
 // The original list is not modified.
 func (el *EventList) Head(n int) *EventList {
-	if n >= len(el.evs) {
-		return &EventList{
-			evs: el.evs,
-		}
+	if el.list == nil {
+		return EmptyList()
 	}
 
-	return &EventList{
-		evs: el.evs[:n],
+	result := EmptyList()
+	iter := el.Iterator()
+	for i := 0; i < n; i++ {
+		event := iter.Next()
+		if event == nil {
+			break
+		}
+		result.PushBack(event)
 	}
+	return result
 }
 
 // RemoveFront removes the first up to n events from the list.
 // Returns the number of events actually removed.
 func (el *EventList) RemoveFront(n int) int {
-	if n > len(el.evs) {
-		n = len(el.evs)
-		el.evs = nil
-		return n
+	if el.list == nil {
+		return 0
 	}
 
-	// TODO: consider reallocating slice to really free up memory
-	el.evs = el.evs[n:]
+	for i := 0; i < n; i++ {
+		if first := el.list.Front(); first != nil {
+			el.list.Remove(first)
+		} else {
+			return i
+		}
+	}
+
 	return n
 }
 
@@ -107,7 +124,21 @@ func (el *EventList) RemoveFront(n int) int {
 // The returned slice only contains pointers to the events in this list, no deep copying is performed.
 // Any modifications performed on the events will affect the contents of both the EventList and the returned slice.
 func (el *EventList) Slice() []*eventpb.Event {
-	return el.evs
+	if el.list == nil {
+		return nil
+	}
+
+	// Create empty result slice.
+	events := make([]*eventpb.Event, 0, el.Len())
+
+	// Populate result slice by appending events one by one.
+	iter := el.Iterator()
+	for event := iter.Next(); event != nil; event = iter.Next() {
+		events = append(events, event)
+	}
+
+	// Return populated result slice.
+	return events
 }
 
 // StripFollowUps collects all follow-up Events of the Events in the list.
@@ -122,7 +153,8 @@ func (el *EventList) StripFollowUps() (*EventList, *EventList) {
 	plainEvents := EventList{}
 
 	// Populate list by follow-up events
-	for _, event := range el.evs {
+	iter := el.Iterator()
+	for event := iter.Next(); event != nil; event = iter.Next() {
 		plainEvent, strippedEvents := Strip(event)
 		plainEvents.PushBack(plainEvent)
 		followUps.PushBackList(strippedEvents)
@@ -135,28 +167,33 @@ func (el *EventList) StripFollowUps() (*EventList, *EventList) {
 // Iterator returns a pointer to an EventListIterator object used to iterate over the events in this list,
 // starting from the beginning of the list.
 func (el *EventList) Iterator() *EventListIterator {
+	if el.list == nil {
+		return &EventListIterator{}
+	}
+
 	return &EventListIterator{
-		evSlice: el.evs,
+		currentElement: el.list.Front(),
 	}
 }
 
 // EventListIterator is an object returned from EventList.Iterator
 // used to iterate over the elements (Events) of an EventList using the iterator's Next method.
 type EventListIterator struct {
-	evSlice []*eventpb.Event
+	currentElement *list.Element
 }
 
 // Next will return the next Event until the end of the associated EventList is encountered.
 // Thereafter, it will return nil.
 func (eli *EventListIterator) Next() *eventpb.Event {
+
 	// Return nil if list has been exhausted.
-	if len(eli.evSlice) == 0 {
+	if eli.currentElement == nil {
 		return nil
 	}
 
 	// Obtain current element and move on to the next one.
-	result := eli.evSlice[0]
-	eli.evSlice = eli.evSlice[1:]
+	result := eli.currentElement.Value.(*eventpb.Event)
+	eli.currentElement = eli.currentElement.Next()
 
 	// Return current element.
 	return result
